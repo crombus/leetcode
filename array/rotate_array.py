1418a605c71d6a427b0c066281234d197ff85b46
crombus <pkundra@redhat.com>
2019-11-07 22:30:54 +0530

    Add rotate Array code with efficency in time and space complexity

---
+++ array/rotate_array.py
@@ -0,0 +1,18 @@
+def rotate_array(nums, k):
+    nums[:k], nums[k:] = nums[len(nums) - k:], nums[:len(nums) - k]
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    nums = [1, 2, 3, 4, 5, 6, 7]
+    k = 3
+    rotate_array(nums, k)
+    print(nums)
