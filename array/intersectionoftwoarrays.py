eef156f352ac9210966e6d62ad51b194c40e6c59
crombus <pkundra@redhat.com>
2019-12-04 16:21:39 +0530

    Add optimised python code for intersection of arrays

--- array/intersectionoftwoarrays.py
+++ array/intersectionoftwoarrays.py
@@ -24,10 +24,28 @@ def intersect(nums1, nums2):
                 y.append(i)
      return y
 
+def intersect_opt(nums1, nums2):
+    large=nums1
+    small=nums2
+    if len(large)<len(small):
+        nums1,nums2=large,small
+    d={}
+    for n in nums1:
+        if n in d:
+            d[n]+=1
+        else:
+            d[n]=1
+    s=[]
+    for n in nums2:
+        if n in d and d[n]>0:
+            s+=[n]
+            d[n]-=1
+    return s
 
 
 if __name__ == '__main__':
     nums1 = [1, 2, 3, 4, 0, 5, 6, 7, 0]
     nums2 = [4,8,4,8,1,1,1,1,1,1]
     print(intersect(nums1, nums2))
+    print(intersect_opt(nums1,nums2))
     
